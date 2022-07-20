82d9907a2e735cfdacf9217fcc3444fc6d85d9a4
HHazard2002 <hugo7.hazard@gmail.com>
2022-07-20 22:48:45 +0100

    draw the pieces

--- chess/piece.py
+++ chess/piece.py
@@ -19,3 +19,31 @@ class Piece:
     def calc_pos(self):
         self.x = SQUARE_SIZE * self.col + SQUARE_SIZE // 2
         self.y = SQUARE_SIZE * self.row + SQUARE_SIZE // 2
+    
+    def draw(self, win):
+        if self.color == BLACK:
+            if self.type == ROOK:
+                win.blit(BLACK_ROOK, (self.x - 47, self.y - 25))
+            if self.type == BISHOP:
+                win.blit(BLACK_BISHOP, (self.x - 40, self.y - 30))
+            if self.type == KNIGHT:
+                win.blit(BLACK_KNIGHT, (self.x - 40, self.y - 30))
+            if self.type == KING:
+                win.blit(BLACK_KING, (self.x - 40, self.y - 30))
+            if self.type == QUEEN:
+                win.blit(BLACK_QUEEN, (self.x - 40, self.y - 30))
+            if self.type == PAWN:
+                win.blit(BLACK_PAWN, (self.x - 35, self.y - 25))
+        else:
+            if self.type == ROOK:
+                win.blit(WHITE_ROOK, (self.x - 35, self.y - 30))
+            if self.type == BISHOP:
+                win.blit(WHITE_BISHOP, (self.x - 40, self.y - 30))
+            if self.type == KNIGHT:
+                win.blit(WHITE_KNIGHT, (self.x - 40, self.y - 30))
+            if self.type == KING:
+                win.blit(WHITE_KING, (self.x - 40, self.y - 30))
+            if self.type == QUEEN:
+                win.blit(WHITE_QUEEN, (self.x - 40, self.y - 30))
+            if self.type == PAWN:
+                win.blit(WHITE_PAWN, (self.x - 35, self.y - 25))
