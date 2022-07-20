9d6c117eff652714e3579e2a0cb08cbd126ddc73
HHazard2002 <hugo7.hazard@gmail.com>
2022-07-20 22:47:42 +0100

    position calculation

--- chess/piece.py
+++ chess/piece.py
@@ -15,3 +15,7 @@ class Piece:
         self.x = 0
         self.y = 0
         self.calc_pos()
+    
+    def calc_pos(self):
+        self.x = SQUARE_SIZE * self.col + SQUARE_SIZE // 2
+        self.y = SQUARE_SIZE * self.row + SQUARE_SIZE // 2
