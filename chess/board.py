d8fe003956ae34776cd46b9da5298b00b9b46c1e
HHazard2002 <hugo7.hazard@gmail.com>
2022-07-22 21:53:51 +0100

    innit

--- chess/board.py
+++ chess/board.py
@@ -2,3 +2,14 @@ import pygame
 from minimax.algorithm import simulate_move
 from .constants import *
 from .piece import Piece
+
+class Board:
+    def __init__(self):
+        self.board = []
+        self.number_eaten_pieces = 0
+        self.eaten_pieces = []
+        self.en_passant = None
+        self.eaten_en_passant = None
+        self.roker_white = True
+        self.roker_black = True
+        self.create_board()
