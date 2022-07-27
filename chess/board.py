0364343b4036bd71e902a961f9a66ffa5afc99c3
HHazard2002 <hugo7.hazard@gmail.com>
2022-07-27 21:41:45 +0100

    commit

--- chess/board.py
+++ chess/board.py
@@ -262,3 +262,9 @@ class Board:
                 moves[(row + 1, col - 1)] = current
             if 7 >= col + 1 and (self.board[row + 1][col + 1] == 0 or self.board[row + 1][col + 1].color != color):
                 moves[(row + 1, col + 1)] = current
+        if 0 <= col - 1:
+            if self.board[row][col - 1] == 0 or self.board[row][col - 1].color != color:
+                moves[(row, col - 1)] = current
+        if 7 >= col + 1:
+            if self.board[row][col + 1] == 0 or self.board[row][col + 1].color != color:
+                moves[(row, col + 1)] = current
