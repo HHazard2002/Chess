01c3d72fa17f1bd41f58a518ffc8c8ecccb91026
HHazard2002 <hugo7.hazard@gmail.com>
2022-07-28 19:35:40 +0100

    bishop left

--- chess/board.py
+++ chess/board.py
@@ -280,3 +280,15 @@ class Board:
                 moves[(7,6)] = current
             
         return moves
+
+    def bishop_up_left(self, current_col, current_row, col, row, color):
+        moves = {}
+        current = self.board[current_row][current_col]
+        if 0 <= col - 1 and 0 <= row - 1:
+            if self.board[row - 1][col - 1] == 0:
+                moves[(row - 1, col - 1)] = current
+                moves.update(self.bishop_up_left(current_col, current_row, col - 1, row - 1, color))
+            elif self.board[row - 1][col - 1].color != color:
+                moves[(row - 1, col - 1)] = current
+
+        return moves
