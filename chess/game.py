d0aee8cce4eae742f66bda9b445749c4e0d3c498
HHazard2002 <hugo7.hazard@gmail.com>
2022-08-26 21:37:44 +0100

    draw with pygame

--- chess/game.py
+++ chess/game.py
@@ -109,3 +109,4 @@ class Game:
   def draw_valid_moves(self, moves):
     for move in moves:
         row, col = move
+        pygame.draw.circle(self.win, BLUE, (col * SQUARE_SIZE + SQUARE_SIZE//2, row * SQUARE_SIZE + SQUARE_SIZE//2), 15)
