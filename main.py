9611edbc4ead22435f2c73eca62281c6196ca6a4
HHazard2002 <hugo7.hazard@gmail.com>
2022-08-31 20:55:38 +0100

    get row col from mouse

--- main.py
+++ main.py
@@ -36,3 +36,4 @@ def main():
                 
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     pos = pygame.mouse.get_pos()
+                    row, col = get_row_col_from_mouse(pos)
